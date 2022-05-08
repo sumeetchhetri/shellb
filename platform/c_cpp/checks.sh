27286148aca0dda8b6b847eae1713385fec98b44
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2022-05-08 15:10:16 +0530

    Snippets shoudl be inline for binary release

--- platform/c_cpp/checks.sh
+++ platform/c_cpp/checks.sh
@@ -38,9 +38,41 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	SHLIB_EXT="dylib"
 fi
 
-test_c_code_=$(<snippets/c_cpp/.test.c)
-test_func_code_=$(<snippets/c_cpp/.testfunc.c)
-test_lib_code_=$(<snippets/c_cpp/.testlib.c)
+test_c_code_=$(cat <<-END
+#include "INC_FILE"
+int main() {return 0;}
+END
+)
+test_func_code_=$(cat <<-END
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C"
+#endif
+char FUNC_NAME();
+
+#if _MSC_VER && !__INTEL_COMPILER
+    #pragma function(accept4)
+#endif
+
+int main(void) {
+#if defined (__stub_accept4) || defined (__stub___accept4)
+  fail fail fail
+#else
+  FUNC_NAME();
+#endif
+
+  return 0;
+}
+END
+)
+test_lib_code_=$(cat <<-END
+int main() {return 0;}
+END
+)
+#test_c_code_=$(<snippets/c_cpp/.test.c)
+#test_func_code_=$(<snippets/c_cpp/.testfunc.c)
+#test_lib_code_=$(<snippets/c_cpp/.testlib.c)
 
 # The additional set of directores to be considered as include/library paths
 EX_DIR=
